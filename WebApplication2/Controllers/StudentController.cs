3d28122ea35b9609679ece22ebd796f3c6cf9e21
Alice <alicefaoro@live.it>
2024-10-23 12:53:11 +0200

    Aggiungere i file di progetto.

---
+++ WebApplication2/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using University.BLogic;
+using University.DataModel;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private string connectionString;
+        private readonly IConfiguration _configuration;
+        StudentManager studentManager = new StudentManager();
+
+        // Iniezione della configurazione nel costruttore
+        public StudentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("SqlDbConnectionString");
+        }
+
+        // GET: api/<UniversityController>
+        [HttpGet]
+        public List<Student> Get()
+        {
+            var students = studentManager.GetStudents(connectionString);
+            return students;
+        }
+
+        // POST api/<UniversityController>
+        [HttpPost]
+        public void Post()
+        {
+            studentManager.AddStudent(connectionString);
+        }
+
+        // PUT api/<UniversityController>/5
+        [HttpPut]
+        public void Put()
+        {
+            studentManager.UpdateStudent(connectionString);
+        }
+
+        // DELETE api/<UniversityController>/5
+        [HttpDelete]
+        public void Delete()
+        {
+            studentManager.DeleteStudent(connectionString);
+        }
+    }
+}
